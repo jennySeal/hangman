cf888d88bba56e2926bdf8ca8efce9c3d53dafdd
Jenny Seal <jennysealft@gmail.com>
2022-02-26 09:16:32 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,696 @@
+const catagories = 
+[["Afghanistan",
+"Albania",
+"Algeria",
+"American Samoa",
+"Andorra",
+"Angola",
+"Antarctica",
+"Argentina",
+"Armenia",
+"Australia",
+"Austria",
+"Azerbaijan",
+"Bahamas",
+"Bahrain",
+"Bangladesh",
+"Barbados",
+"Belarus",
+"Belgium",
+"Belize",
+"Benin",
+"Bermuda",
+"Bhutan",
+"Bolivia",
+"Botswana",
+"Brazil",
+"Bulgaria",
+"Burkina Faso",
+"Burundi",
+"Cambodia",
+"Cameroon",
+"Canada",
+"Cape Verde",
+"Cayman Islands",
+"Chad",
+"Chile",
+"China",
+"Christmas Island",
+"Colombia",
+"Congo",
+"Cook Islands",
+"Costa Rica",
+"Cote D'Ivoire",
+"Croatia",
+"Cuba",
+"Cyprus",
+"Czech Republic",
+"Denmark",
+"Dominican Republic",
+"Ecuador",
+"Egypt",
+"El Salvador",
+"Equatorial Guinea",
+"Eritrea",
+"Estonia",
+"Ethiopia",
+"Falkland Islands",
+"Faroe Islands",
+"Fiji",
+"Finland",
+"France",
+"French Guiana",
+"French Polynesia",
+"Gabon",
+"Gambia",
+"Georgia",
+"Germany",
+"Ghana",
+"Gibraltar",
+"Greece",
+"Greenland",
+"Grenada",
+"Guadeloupe",
+"Guam",
+"Guatemala",
+"Guernsey",
+"Guinea",
+"Guyana",
+"Haiti",
+"Vatican City",
+"Honduras",
+"Hong Kong",
+"Hungary",
+"Iceland",
+"India",
+"Indonesia",
+"Iran",
+"Iraq",
+"Ireland",
+"Israel",
+"Italy",
+"Jamaica",
+"Japan",
+"Jersey",
+"Jordan",
+"Kazakhstan",
+"Kenya",
+"Kiribati",
+"North Korea",
+"South Korea",
+"Kosovo",
+"Kuwait",
+"Kyrgyzstan",
+"Latvia",
+"Lebanon",
+"Lesotho",
+"Liberia",
+"Liechtenstein",
+"Lithuania",
+"Luxembourg",
+"Macao",
+"Macedonia",
+"Madagascar",
+"Malawi",
+"Malaysia",
+"Maldives",
+"Mali",
+"Malta",
+"Mauritania",
+"Mauritius",
+"Mexico",
+"Moldova",
+"Monaco",
+"Mongolia",
+"Montenegro",
+"Montserrat",
+"Morocco",
+"Mozambique",
+"Myanmar",
+"Namibia",
+"Nepal",
+"Netherlands",
+"New Zealand",
+"Nicaragua",
+"Niger",
+"Nigeria",
+"Norway",
+"Oman",
+"Pakistan",
+"Palau",
+"Palestine",
+"Panama",
+"Papua New Guinea",
+"Paraguay",
+"Peru",
+"Philippines",
+"Poland",
+"Portugal",
+"Puerto Rico",
+"Qatar",
+"Romania",
+"Russian Federation",
+"Rwanda",
+"Saint Lucia",
+"Samoa",
+"San Marino",
+"Saudi Arabia",
+"Senegal",
+"Serbia",
+"Seychelles",
+"Sierra Leone",
+"Singapore",
+"Slovakia",
+"Slovenia",
+"Solomon Islands",
+"Somalia",
+"South Africa",
+"South Sudan",
+"Spain",
+"Sri Lanka",
+"Sudan",
+"Suriname",
+"Swaziland",
+"Sweden",
+"Switzerland",
+"Syria",
+"Taiwan",
+"Tajikistan",
+"Tanzania",
+"Thailand",
+"Togo",
+"Tonga",
+"Trinidad and Tobago",
+"Tunisia",
+"Turkey",
+"Turkmenistan",
+"Tuvalu",
+"Uganda",
+"Ukraine",
+"United Arab Emirates",
+"United Kingdom",
+"United States",
+"Uruguay",
+"Uzbekistan",
+"Venezuela",
+"Vietnam",
+"Virgin Islands",
+"Western Sahara",
+"Yemen",
+"Zambia",
+"Zimbabwe"
+],
+["Afghanistan",
+"Albania",
+"Algeria",
+"American Samoa",
+"Andorra",
+"Angola",
+"Antarctica",
+"Argentina",
+"Armenia",
+"Australia",
+"Austria",
+"Azerbaijan",
+"Bahamas",
+"Bahrain",
+"Bangladesh",
+"Barbados",
+"Belarus",
+"Belgium",
+"Belize",
+"Benin",
+"Bermuda",
+"Bhutan",
+"Bolivia",
+"Botswana",
+"Brazil",
+"Bulgaria",
+"Burkina Faso",
+"Burundi",
+"Cambodia",
+"Cameroon",
+"Canada",
+"Cape Verde",
+"Cayman Islands",
+"Chad",
+"Chile",
+"China",
+"Christmas Island",
+"Colombia",
+"Congo",
+"Cook Islands",
+"Costa Rica",
+"Cote D'Ivoire",
+"Croatia",
+"Cuba",
+"Cyprus",
+"Czech Republic",
+"Denmark",
+"Dominican Republic",
+"Ecuador",
+"Egypt",
+"El Salvador",
+"Equatorial Guinea",
+"Eritrea",
+"Estonia",
+"Ethiopia",
+"Falkland Islands",
+"Faroe Islands",
+"Fiji",
+"Finland",
+"France",
+"French Guiana",
+"French Polynesia",
+"Gabon",
+"Gambia",
+"Georgia",
+"Germany",
+"Ghana",
+"Gibraltar",
+"Greece",
+"Greenland",
+"Grenada",
+"Guadeloupe",
+"Guam",
+"Guatemala",
+"Guernsey",
+"Guinea",
+"Guyana",
+"Haiti",
+"Vatican City",
+"Honduras",
+"Hong Kong",
+"Hungary",
+"Iceland",
+"India",
+"Indonesia",
+"Iran",
+"Iraq",
+"Ireland",
+"Israel",
+"Italy",
+"Jamaica",
+"Japan",
+"Jersey",
+"Jordan",
+"Kazakhstan",
+"Kenya",
+"Kiribati",
+"North Korea",
+"South Korea",
+"Kosovo",
+"Kuwait",
+"Kyrgyzstan",
+"Latvia",
+"Lebanon",
+"Lesotho",
+"Liberia",
+"Liechtenstein",
+"Lithuania",
+"Luxembourg",
+"Macao",
+"Macedonia",
+"Madagascar",
+"Malawi",
+"Malaysia",
+"Maldives",
+"Mali",
+"Malta",
+"Mauritania",
+"Mauritius",
+"Mexico",
+"Moldova",
+"Monaco",
+"Mongolia",
+"Montenegro",
+"Montserrat",
+"Morocco",
+"Mozambique",
+"Myanmar",
+"Namibia",
+"Nepal",
+"Netherlands",
+"New Zealand",
+"Nicaragua",
+"Niger",
+"Nigeria",
+"Norway",
+"Oman",
+"Pakistan",
+"Palau",
+"Palestine",
+"Panama",
+"Papua New Guinea",
+"Paraguay",
+"Peru",
+"Philippines",
+"Poland",
+"Portugal",
+"Puerto Rico",
+"Qatar",
+"Romania",
+"Russian Federation",
+"Rwanda",
+"Saint Lucia",
+"Samoa",
+"San Marino",
+"Saudi Arabia",
+"Senegal",
+"Serbia",
+"Seychelles",
+"Sierra Leone",
+"Singapore",
+"Slovakia",
+"Slovenia",
+"Solomon Islands",
+"Somalia",
+"South Africa",
+"South Sudan",
+"Spain",
+"Sri Lanka",
+"Sudan",
+"Suriname",
+"Swaziland",
+"Sweden",
+"Switzerland",
+"Syria",
+"Taiwan",
+"Tajikistan",
+"Tanzania",
+"Thailand",
+"Togo",
+"Tonga",
+"Trinidad and Tobago",
+"Tunisia",
+"Turkey",
+"Turkmenistan",
+"Tuvalu",
+"Uganda",
+"Ukraine",
+"United Arab Emirates",
+"United Kingdom",
+"United States",
+"Uruguay",
+"Uzbekistan",
+"Venezuela",
+"Vietnam",
+"Virgin Islands",
+"Western Sahara",
+"Yemen",
+"Zambia",
+"Zimbabwe"
+],
+["plymouth argyle", "tottenham hotspur", "aston villa", "nottingham forest", "arsenal", "coventry city", "liverpool", "manchester city", "everton", "newcastle united", "celtic", "kilmarnock", "rangers", "st mirren", "st johnstone", "norwich city", "leeds united", "sheffield wednesday", "leyton orient", "oldham athletic", "carlisle united", "brentford", "chelsea", "crystal palace", "wolverhampton wanderers", "west ham united", "queens park rangers", "west bromwich albion", "luton town", "accrington stanley", "wigan athletic", "oldham athletic", "heart of midlothian", "real madrid"], 
+["polar bear", "elephant", "australian kelpie", "kangaroo", "squirrel", "gerbil", "hare", "seal", "horse", "german shepherd", "tasmanian devil", "caribou", "lion", "jaguar", "puma", "panda", "guinea pig", "blue whale", "giraffe", "moose", "chihuahua", "snow leopard", "anteater", "crocodile", "raccoon", "quokka", "coyote", "water buffalo", "pine marten"], 
+["lilac", "maroon", "crimson", "amber", "jade", "rainbow", "fluorescent pink", "bronze", "silver", "navy blue", "black", "mustard", "yellow", "orange", "aquamarine", "cream", "beige", "purple", "scarlet", "aubergine", "bottle green", "burnt orange", "rose gold", "fuchsia", "peach", "teal"], 
+["pelican", "pigeon", "vulture", "golden eagle", "kestril", "peregrine falcon", "robin", "goldfinch", "peacock", "albatross", "budgie", "parrot", "toucan", "penguin", "flamingo", "seagull", "sparrow", "blackbird", "stork", "crane", "barn owl", "buzzard", "coot", "cuckoo", "yellowhammer", "turtle dove", "nightingale"], 
+["yoghurt", "cheesecake", "pasta", "pizza", "chips", "crisps", "chocolate", "biscuits", "watermelon", "mango", "strawberries", "cherries", "courgettes", "butternut squash", "sausage", "chicken", "quorn mince", "tuna fish", "burger", "sushi", "cauliflower", "macaroni cheese", "lasagne", "steak", "chow mein", "chicken tikka masala", "apricot", "yum yum", "rocket"]]
+
+catagoryNames = ['Countries', 'Countries', 'Football Teams', 'Animals', 'Colours', 'Birds', 'Food']
+
+canvas =  function(){
+    
+    myStickman = document.getElementById("stickman");
+    
+    context = myStickman.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = " hsl(49, 51%, 47%)";
+    context.shadowColor =  'hsl(240, 2%, 23%)';
+    context.shadowBlur = 1;
+    context.lineWidth = 5;
+    context.shadowOffsetX = 0.25;
+  };
+  
+    head = function(){
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.arc(130, 25, 10, 0, Math.PI*2, true);
+      context.stroke();
+    }
+    
+  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke(); 
+  }
+  
+   frame1 = function() {
+     draw (70, 150, 250, 150);
+   };
+   
+   frame2 = function() {
+     draw (70, 0, 70, 500);
+   };
+  
+   frame3 = function() {
+     draw (70, 0, 130, 0);
+   };
+  
+   frame4 = function() {
+     draw (130, 0, 130, 15);
+   };
+  
+   torso = function() {
+     draw (130, 36, 130, 70);
+   };
+  
+   rightArm = function() {
+     draw (130, 46, 170, 50);
+   };
+  
+   leftArm = function() {
+     draw (130, 46, 90, 50);
+   };
+  
+   rightLeg = function() {
+     draw (130, 70, 160, 100);
+   };
+  
+   leftLeg = function() {
+     draw (130, 70, 110, 100);
+   };
+  
+  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+
+
+let livesLeft = 10;
+const livesTile = document.querySelector('[class=lives-tile]')
+const alert = document.querySelector([`[class='alert'`])
+    
+
+const setUpLives = () => {
+livesTile.innerText = `${livesLeft}`
+}
+
+const animate = (livesLeft) => {
+    let drawMe = livesLeft-1 
+    drawArray[drawMe]();
+  }
+
+const getRandomCatagory = () => {
+    let max = catagories.length;
+    let min = 0;
+    return Math.floor(Math.random() * (max - min) + min);
+ } 
+ 
+const getHangmanWord = (catagory) => {
+    let max = catagory.length
+    let min = 0;
+    let wordPosition = Math.floor(Math.random() * (max-min) + min);
+    return catagory[wordPosition]
+}
+
+
+
+const setUpWord = () => {
+let hangmanWordArray = hangmanWord.split(" ")
+
+for (j=0; j<hangmanWordArray.length; j++) {
+    let gridRow = document.getElementsByClassName(`guess-row${j}`)
+    gridRow[0].style.display = 'grid'
+    gridRow[0].style.gridTemplateColumns = `repeat(${hangmanWordArray[j].length}, minmax(auto, 2.5em))`
+
+for (i=0; i<hangmanWordArray[j].length;i++) {
+    let tile;
+    let letter = hangmanWordArray[j][i]
+         tile = document.createElement('div')
+         tile.dataset.letter = (letter === " ") ? " " : letter.toLowerCase();
+         tile.dataset.tag = (letter === " ") ? " " : letter;
+         tile.className = (letter === " ") ? 'tile.blank' : 'tile';
+    gridRow[0].insertAdjacentElement('beforeend', tile)
+
+}
+}
+}
+
+const startGame = () => {
+    document.addEventListener("click", handleMouseClick);
+    document.addEventListener("keydown", handleKeyPress)
+   
+}
+
+const stopGame = () => {
+    document.removeEventListener("click", handleMouseClick);
+    document.removeEventListener("keydown", handleKeyPress)
+    const playAgain = document.getElementById('playagain')
+    playAgain.style.display = 'flex'
+}
+
+const handleMouseClick = (e) => {
+    if (e.target.matches("[data-key]")) {
+        pressKey(e.target.dataset.key)
+        return
+    }
+}
+
+
+const handleKeyPress = (e) => {
+    if (e.key.match(/^[a-z]$/)) {
+        pressKey(e.key)
+        return
+    }
+}
+
+const pressKey = (key) => {
+    key = key.toUpperCase()
+    let hangmanWordUpper = hangmanWord.toUpperCase()
+    
+    let pressedKey = document.querySelector(`[data-key=${key}]`);
+
+    if (hangmanWordUpper.includes(key)) {
+
+    correctGuess(pressedKey, key)
+    let congratulations;
+    let emptyTiles = document.getElementsByClassName(['tile'])
+        if (!emptyTiles.length) {
+                danceTiles()
+                console.log(livesLeft)
+                switch(true) {
+                    case (livesLeft > 8): congratulations = "Impressive";
+	                break;
+                    case (livesLeft > 5): congratulations = "Awesome";
+	                break;
+                    case (livesLeft > 3): congratulations = "Well done";
+	                break;
+                    case (livesLeft > 0): congratulations = "Phew";
+	                break;
+  }
+ 
+                
+                showAlert(congratulations)
+                stopGame()
+
+        }        
+        
+    } else {
+        pressedKey.style.backgroundColor = 'hsl(240, 2%, 23%)'
+        canvas();
+
+        animate(livesLeft--)
+
+        if (livesLeft > 5) {
+            livesTile.style.backgroundColor = 'hsl(115, 29%, 43%)'
+        
+        } 
+        else if (livesLeft === 1) {
+            livesTile.style.backgroundColor = '#ff000056';
+        }
+        
+        else if (livesLeft === 0) {
+            stopGame()
+            showAlert('Computer wins!')
+            let remainingKeys = document.querySelectorAll([`[class='tile'`])
+            remainingKeys.forEach(remainingKey => {
+            remainingKey.className = 'correct flip'
+            
+            remainingKey.innerText = remainingKey.dataset.tag;
+            remainingKey.style.backgroundColor = 'hsl(49, 51%, 47%)';
+            remainingKey.addEventListener('transitionend', () => {
+                tile.classList.remove('flip')
+            })})
+        } else {
+            livesTile.style.backgroundColor = 'hsl(49, 51%, 47%)' 
+        }
+
+        livesTile.innerText = `${livesLeft}`
+        livesTile.className = 'correct flip'
+        livesTile.addEventListener(
+            'animationend', () => {
+                livesTile.classList.remove('flip')
+    })}}
+
+
+
+const showCatagory = () => {
+    let hintButton = document.getElementById('hintButton')
+    if (hintButton.innerText === 'HINT') {
+        hintButton.innerText = catagoryNames[catagoryPlace] 
+        hintButton.style.backgroundColor = 'hsl(240, 2%, 23%)'
+       } else {
+        hintButton.innerText = 'HINT' 
+        hintButton.style.backgroundColor = 'hsl(115, 29%, 43%)'
+}
+
+}
+
+function danceTiles() {
+    const tiles = document.getElementsByClassName('correct flip')
+    const tileArray = [].slice.call(tiles)
+        
+        for (let i=0; i<tileArray.length; i++) {
+        sleep(1000).then(makeTilesDance(tiles[i]))
+
+        }
+    
+}
+         
+    function makeTilesDance(tile) {
+        if (tile !== undefined) {
+        tile.className = 'tile dance'
+        tile.style.backgroundColor = 'hsl(115, 29%, 43%)';
+    } 
+    }
+    function sleep(time) {
+        return new Promise(resolve => setTimeout(()=>resolve(), time));
+      }
+ 
+
+
+
+const correctGuess = (pressedKey, key) => {
+    pressedKey.className = "key right";
+    pressedKey.style.backgroundColor = 'hsl(115, 29%, 43%)'
+
+    let correctKeys = document.querySelectorAll([`[data-letter=${key.toLowerCase()}`])
+    correctKeys.forEach(correctKey => {
+    correctKey.className = 'correct flip'
+    correctKey.innerText = key;
+    correctKey.addEventListener('transitionend', () => {
+        tile.classList.remove('flip')
+    
+        
+
+    })})
+}
+
+const windowReload = () => {
+    location.reload()
+}
+
+const showAlert = (word) => {
+    alert.innerText = word;
+    alert.style.display = 'flex';
+
+}
+
+
+
+window.onload = setUpLives()
+const catagoryPlace = getRandomCatagory()
+const hangmanWord = getHangmanWord(catagories[catagoryPlace])
+setUpWord()
+
+startGame()
